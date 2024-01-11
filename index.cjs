6eb507b79892d6b20f529f253f13aedb934b0a8d
Shadi Abu Hilal <shadiabuhilal@Shadis-MacBook-Pro-2.local>
2024-01-10 16:53:25 -0800

    refactor: freeze 'QueryOperators' object and update docs

--- index.cjs
+++ index.cjs
@@ -1,84 +1,31 @@
 'use strict';
 
 /**
- * @private
- * @description Supported JSON content type values ['application/json', 'application/problem+json', 'application/vnd.api+json', 'application/hal+json'].
+ * Collection of query operators for Firestore queries.
+ *
+ * @typedef {Object} QueryOperators
+ * @property {string} lessThan - Represents the less than operator (`<`).
+ * @property {string} lessThanOrEqualTo - Represents the less than or equal to operator (`<=`).
+ * @property {string} equalTo - Represents the equal to operator (`==`).
+ * @property {string} greaterThan - Represents the greater than operator (`>`).
+ * @property {string} greaterThanOrEqualTo - Represents the greater than or equal to operator (`>=`).
+ * @property {string} notEqualTo - Represents the not equal to operator (`!=`).
+ * @property {string} arrayContains - Represents the array contains operator (`array-contains`).
+ * @property {string} arrayContainsAny - Represents the array contains any operator (`array-contains-any`).
+ * @property {string} in - Represents the "in" operator (`in`).
+ * @property {string} notIn - Represents the not in operator (`not-in`).
  */
-const jsonContentTypes = [
-    'application/json',
-    'application/problem+json',
-    'application/vnd.api+json',
-    'application/hal+json'
-];
+const QueryOperators = Object.freeze({
+    lessThan: '<',
+    lessThanOrEqualTo: '<=',
+    equalTo: '==',
+    greaterThan: '>',
+    greaterThanOrEqualTo: '>=',
+    notEqualTo: '!=',
+    arrayContains: 'array-contains',
+    arrayContainsAny: 'array-contains-any',
+    in:'in',
+    notIn: 'not-in'
+});
 
-/**
- * @function
- * @description Function that check if current request is a json request or not.
- * @param {Object} req - Http request object
- * @param {Object} req.headers - Http request headers object
- * @param {string} req.headers."content-type" - Http request content-type header
- * @returns {boolean}
- */
-const isJsonReq = (req) => {
-    const contentType = req && req.headers && req.headers['content-type'];
-
-    const normalizedContentType = contentType && contentType.toLowerCase();
-
-    if (normalizedContentType && jsonContentTypes.some((value) => normalizedContentType.includes(value))) {
-        return true;
-    }
-
-    return false;
-};
-
-const checkMethod = (req, method) => {
-    const normalizedReqMethod  = req && req.method && req.method && req.method.toLowerCase();
-
-    if (normalizedReqMethod === method) {
-        return true;
-    }
-
-    return false;
-};
-
-/**
- * @function
- * @description Function that check if current request is a http GET method request or not.
- * @param {Object} req - Http request object
- * @param {string} req.method - Http request method string
- * @returns {boolean}
- */
-const isGetReq = (req) => checkMethod(req, 'get');
-
-/**
- * @function
- * @description Function that check if current request is a http POST method request or not.
- * @param {Object} req - Http request object
- * @param {string} req.method - Http request method string
- * @returns {boolean}
- */
-const isPostReq = (req) => checkMethod(req, 'post');
-
-/**
- * @function
- * @description Function that check if current request is a http PUT method request or not.
- * @param {Object} req - Http request object
- * @param {string} req.method - Http request method string
- * @returns {boolean}
- */
-const isPutReq = (req) => checkMethod(req, 'put');
-
-/**
- * @function
- * @description Function that check if current request is a http DELETE method request or not.
- * @param {Object} req - Http request object
- * @param {string} req.method - Http request method string
- * @returns {boolean}
- */
-const isDeleteReq = (req) => checkMethod(req, 'delete');
-
-exports.isDeleteReq = isDeleteReq;
-exports.isGetReq = isGetReq;
-exports.isJsonReq = isJsonReq;
-exports.isPostReq = isPostReq;
-exports.isPutReq = isPutReq;
+exports.QueryOperators = QueryOperators;
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 /**
- * @description Firestore query operations.
+ * Collection of query operators for Firestore queries.
+ *
+ * @typedef {Object} QueryOperators
+ * @property {string} lessThan - Represents the less than operator (`<`).
+ * @property {string} lessThanOrEqualTo - Represents the less than or equal to operator (`<=`).
+ * @property {string} equalTo - Represents the equal to operator (`==`).
+ * @property {string} greaterThan - Represents the greater than operator (`>`).
+ * @property {string} greaterThanOrEqualTo - Represents the greater than or equal to operator (`>=`).
+ * @property {string} notEqualTo - Represents the not equal to operator (`!=`).
+ * @property {string} arrayContains - Represents the array contains operator (`array-contains`).
+ * @property {string} arrayContainsAny - Represents the array contains any operator (`array-contains-any`).
+ * @property {string} in - Represents the "in" operator (`in`).
+ * @property {string} notIn - Represents the not in operator (`not-in`).
  */
-export const QueryOperators = {
+export const QueryOperators = Object.freeze({
     lessThan: '<',
     lessThanOrEqualTo: '<=',
     equalTo: '==',
@@ -12,4 +24,4 @@ export const QueryOperators = {
     arrayContainsAny: 'array-contains-any',
     in:'in',
     notIn: 'not-in'
-};
+});
